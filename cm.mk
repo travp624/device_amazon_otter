b0b42ffcc4b0bdb6dad71a4f96eb522523fc3f13
Hashcode <hashcode0f@gmail.com>
2011-12-20 21:23:40 -0800

    changes

--- cm.mk
+++ cm.mk
@@ -1,39 +1,43 @@
-# Copyright (C) 2011 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Inherit device configuration for Droid3.
+$(call inherit-product, device/amazon/otter/full_otter.mk)
+
+# Inherit some common cyanogenmod stuff.
+$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+
+# Inherit some common CM stuff.
+#$(call inherit-product, vendor/cm/config/gsm.mk)
 
 #
-# This file is the build configuration for a full Android
-# build for toro hardware. This cleanly combines a set of
-# device-specific aspects (drivers) with a device-agnostic
-# product configuration (apps). Except for a few implementation
-# details, it only fundamentally contains two inherit-product
-# lines, full and toro, hence its name.
+# Setup device specific product configuration.
 #
-
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-# This is where we'd set a backup provider if we had one
-#$(call inherit-product, device/sample/products/backup_overlay.mk)
-# Inherit from otter device
-$(call inherit-product, device/amazon/otter/device.mk)
-$(call inherit-product, vendor/aokp/configs/common_tablet.mk)
-$(call inherit-product-if-exists, vendor/amazon/otter/otter-vendor.mk)
-
-# Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := cm_otter
-PRODUCT_DEVICE := otter
 PRODUCT_BRAND := Android
-PRODUCT_MODEL := Amazon Kindle Fire
+PRODUCT_DEVICE := otter
+PRODUCT_MODEL := otter
 PRODUCT_MANUFACTURER := Amazon
+PRODUCT_SFX := ics
+
+# Release name and versioning
 PRODUCT_RELEASE_NAME := KFire
+
+UTC_DATE := $(shell date +%s)
+DATE := $(shell date +%Y%m%d)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+   BUILD_ID=6.2.0_G1011999 \
+   PRODUCT_NAME=${PRODUCT_DEVICE}_${PRODUCT_SFX} \
+   BUILD_NUMBER=${DATE} \
+   TARGET_DEVICE=amz_otter \
+   BUILD_DISPLAY_ID=6.2.0_G1011999 \
+   BUILD_FINGERPRINT=amazon/otter_ics/amz_otter:4.0.3/6.2.0_G10111999/${BUILD_NUMBER}:user/release-keys \
+   PRIVATE_BUILD_DESC="amz_otter-user 4.0.3 6.2.0_G10111999 "${BUILD_NUMBER}" release-keys" \
+   PRODUCT_BRAND=verizon \
+   BUILD_UTC_DATE= \
+   TARGET_BUILD_TYPE=user \
+   BUILD_VERSION_TAGS=release-keys \
+   USER=hashcode \
+   BUILD_HOST=hashcode-unn \
+   PRODUCT_DEFAULT_LANGUAGE=en \
+   PRODUCT_DEFAULT_REGION=US \
+
+
